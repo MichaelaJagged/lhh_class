f94772272d1ccf064e44de8c9f460252c7421dbd
Michaela <michaelagoeglein@gmail.com>
2019-02-27 10:07:25 -0800

    Fix shuffling and start dealing.

--- deal.js
+++ deal.js
@@ -1,5 +1,11 @@
 const columnCount = 7;
 
 function deal() {
-    alert("Deal!");
+this.columns[0].append(this.cards[0])
+this.columns[1].append(this.cards[1])
+this.columns[2].append(this.cards[2])
+this.columns[3].append(this.cards[3])
+this.columns[4].append(this.cards[4])
+this.columns[5].append(this.cards[5])
+this.columns[6].append(this.cards[6])
 }
--- shuffle.js
+++ shuffle.js
@@ -1,3 +1,7 @@
 function shuffle() {
-    alert("shuffle");
+    for (let i = 0; i < 52; i++) {
+        let j = random(i, 51)
+        this.cards[i].swap(this.cards[j]);
+    }
 }
+

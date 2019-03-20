2f995a11d7791f9533ebdf147befbd172bf20a42
Michaela <michaelagoeglein@gmail.com>
2019-03-20 10:46:35 -0700

    Start enforcing the rules on moving cards around.

--- cardClicked.js
+++ cardClicked.js
@@ -1,3 +1,28 @@
 function cardClicked(card, column, lastCard, lastColumn) {
-    alert("You clicked " + card.name() + " in column " + column.id);
+    if (card.reversed) {
+        card.reversed = false;
+        return;
+    }
+    if (lastCard == null) {
+       
+
+        this.remember(card, column);
+        card.selected = true;
+    }
+
+    else {
+    if (card.color == lastCard.color) {
+        return;
+    }
+    
+    if (card.value != lastCard.value+1) {
+        return;
+    }
+        let cards = lastColumn.popFrom(lastCard);
+        column.append(cards);
+        this.forget();
+        lastCard.selected = false
+    
+    }
+
 }
